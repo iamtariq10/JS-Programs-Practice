3bced284a437dd9b4a6be32d3939d583a36d5781
Mohammad Tariq <m.faixal16212@gmail.com>
2021-02-27 14:37:42 +0500

    Programs done first 14/39

---
+++ app.js
@@ -0,0 +1,210 @@
+//PROGRAM 1
+
+// let num1 = prompt();
+// let num2 = prompt();
+
+// if(num1>num2){
+//     console.log(`Number ${num1} is greater than the ${num2}`)
+// }
+// else{
+//     console.log(`Number ${num2}  is greater than ${num1}`)
+// }
+
+//PROGRAM 2
+
+
+// let number1 = prompt();
+// if(number1>0){
+//     console.log(`${number1} is positive`);
+// }
+// else if(number1==0){
+//     console.log(`${number1} is zero`);
+
+// }
+// else{
+//     console.log(`${number1} is negative`);
+
+// }
+
+
+
+//PROGRAM 3
+
+// let numb1 = prompt();
+
+// if(numb1 % 5 ==0){
+//     console.log(`${numb1} is divisible by 5`)
+
+// }
+// else if(numb1 % 11 ==0 ){
+//     console.log(`${numb1} is divisible by 11`)
+
+// }
+// else{
+//     console.log(`${numb1} is not divisible by 5 and 11`)
+
+// }
+
+
+//PROGRAM 4
+
+
+
+// let aNumber = prompt("Enter your number:");
+
+// if(aNumber % 2 ==0){
+//     console.log(`${aNumber} is even`)
+
+// }
+// else if(aNumber % 2 ==1 ){
+//     console.log(`${aNumber} is odd`)
+
+// }
+// else{
+//     console.log(`${aNumber} is not even nor odd`)
+
+// }
+
+//PROGRAM 5
+
+// let year = prompt("Enter your year:");
+// if(year % 400 ==0 || year % 4 ==0 && year % 100 !=0  )
+// {
+//     console.log(`${year} is a leap year`);
+// }
+// else{
+//     console.log(`${year} is not a leap year`);
+
+// }
+
+
+//PROGRAM 6
+
+
+// let input = prompt("Enter your value:")
+// console.log("Lenght of the input value is", input.length);
+
+
+//PROGRAM 7
+
+
+// let num1 = prompt();
+// let num2 = prompt();
+// let num3 = prompt();
+
+// if(num1>num2 && num1>num3){
+//     console.log(`Number ${num1} is greater than the ${num2} and ${num3}`)
+// }
+// else if(num2>num1 && num2>num3){
+//     console.log(`Number ${num2}  is greater than ${num1} and ${num3}`)
+// }
+// else if(num3>num1 && num3>num2){
+//     console.log(`Number ${num3}  is greater than ${num1} and ${num2}`)   
+// }
+// else{
+//     console("Error");
+// }
+
+
+
+//PROGRAM 8
+
+// let char = prompt("Enter your char:");
+
+// if (char=='a' || char=='e' || char=='i' || char=='o' || char=='u'){
+//     console.log(`${char} is vowel`);
+// }
+
+// else{
+//     console.log(`${char} is consonant`);
+// }
+
+
+//PROGRAM 9
+
+
+// let char = prompt("Enter your char:");
+
+// if (char>='a' && char<='z'){
+//     console.log(`${char} is alphabet`);
+// }
+
+// else{
+//     console.log(`${char} is not alphabet`);
+// }
+
+
+
+//PROGRAM 10
+// let input = prompt("Enter your input:");
+
+// if (input>='a' && input<='z'){
+//     console.log(`${input} is alphabet`);
+// }
+
+// else if(input>=0 || input<=0){
+//     console.log(`${input} is Number `);
+// }
+
+// else{
+//     console.log(`${input} is Special character `);
+// }
+
+
+//PROGRAM 11
+
+
+// let str1 = "This is string 1 ";
+// let str2 = str1.concat("This is string 2");
+
+// console.log("Concatinated string: ", str2)
+
+
+//PROGRAM 12
+
+
+// let input1 =prompt("Your input1: ");
+// let input2 =prompt("Your input2: ");
+
+// if(input1==input2){
+//     console.log(`${input1} is matched with ${input2}`)
+// }
+
+// else{
+//     console.log(`${input1} is not matched with ${input2}`)
+// }
+
+//PROGRAM 13
+
+// let str = prompt("Enter your input: ");
+
+// let toUpperCase = str.toUpperCase();
+
+// console.log("UpperCase string is: ",toUpperCase);
+
+
+
+//PROGRAM 14
+
+// let str = prompt("Enter your input: ");
+
+// let toLowerCase = str.toLowerCase();
+
+// console.log("UpperCase string is: ",toLowerCase);
+
+
+//PROGRAM 15
+
+
+
+
+
+
+
+
+
+
+
+
+
+
